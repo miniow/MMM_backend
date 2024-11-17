4e324742d52c2e7441636e1b5a59581ef15ac342
miniow <zymcioxx@gmail.com>
2024-11-17 22:47:20 +0100

    first commit

---
+++ Domain/Entities/DataPipeline.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Xml.Serialization;
+
+namespace Domain.Entities
+{
+    namespace Domain.Entities
+    {
+        public class DataPipeline
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public ICollection<DataSource> DataSources { get; set; } = new List<DataSource>();
+        }
+    }
+    public class Connection
+    {
+        public object From { get; set; }
+        public object To { get; set; }
+    }
+    public abstract class DataSource
+    {
+        public Guid Id { get; set; }
+        public Guid DataPipelineId { get; set; }
+        public DataPipeline DataPipeline { get; set; }
+        public string? Columns { get; set; }
+    }
+
+    public class FileSource : DataSource
+    {
+        public string? LastFileName { get; set; } 
+    }
+
+}
---
+++ Infrastructure/Data/UserDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class User : IdentityUser
+    {
+        public string? Initials { get; set; }
+
+    }
+
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().Property(u => u.Initials).HasMaxLength(5);
+
+            builder.HasDefaultSchema("identity");
+        }
+    }
+}
